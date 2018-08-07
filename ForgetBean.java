ee62d9137195350b3a73e4dcceab68dd0c6cd973
cupcake9 <41993844+cupcake9@users.noreply.github.com>
2018-08-07 18:25:54 +0530

    lti.hola.bean

---
+++ ForgetBean.java
@@ -0,0 +1,24 @@
+package lti.hola.bean;
+/**
+ * 
+ * @author tasneem,valli,vanshree
+ * @version 1.0
+ *
+ */
+public class ForgetBean {
+private String email;
+private String movie;
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getMovie() {
+	return movie;
+}
+public void setMovie(String movie) {
+	this.movie = movie;
+}
+
+}
